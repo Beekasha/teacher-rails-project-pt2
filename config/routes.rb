82e1c672c2db22339d9c03b1ab15ae51fd054029
Beekasha <brian.j.kleiner@gmail.com>
2020-04-21 16:53:26 -0400

    add registrations controller to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :comments
   root to: 'pages#home'
-  devise_for :teachers
+  devise_for :teachers, :controllers => {registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
