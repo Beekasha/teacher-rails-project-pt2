014c4f6299471d52e2b30d6537c4b63b92a38133
Beekasha <brian.j.kleiner@gmail.com>
2020-04-21 16:47:24 -0400

    add sign up params within registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,5 @@
 class RegistrationsController < ApplicationController
+    def sign_up_params
+        params.require(:teacher).permit(:email, :password, :password_confirmation, :name)
+    end
 end
