11df0c80c351f31f6b7c0af20890ef7f12dc8850
Beekasha <brian.j.kleiner@gmail.com>
2020-04-21 17:04:34 -0400

    sign in fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,5 @@ class RegistrationsController < ApplicationController
     def sign_up_params
         params.require(:teacher).permit(:email, :password, :password_confirmation, :name)
     end
+
 end
