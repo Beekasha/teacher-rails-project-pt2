68ddaaf1cd93140dc89bf2df488b493babb5ef98
Beekasha <brian.j.kleiner@gmail.com>
2020-04-21 22:20:27 -0400

    added relationships of teacher / comments

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,12 @@
 class Teacher < ApplicationRecord
+  has_many :klasses
+  has_many :comments
+  has_many :documents, through: :klasses
+    
+  has_many :students, through: :klasses
+  has_many :parents, through: :students
+
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
