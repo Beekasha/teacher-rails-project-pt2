2f5a62180281d8405a71e76aa699551935406b80
Beekasha <brian.j.kleiner@gmail.com>
2020-04-30 01:03:14 -0400

    fleshing out student views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,9 @@ class StudentsController < ApplicationController
         #this needs to change to students that share classes with the teacher
         @students = Student.all
     end
+
+    def show
+        @student = Student.find_by(id: params[:id])
+        @comments = @student.comments.all
+    end
 end
