412ab3ec1a05a13127492e8ed0ee126f17ae7bb5
Beekasha <brian.j.kleiner@gmail.com>
2020-05-02 16:06:42 -0400

    custom url added for scope method

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   resources :comments
   root to: 'pages#home'
   devise_for :teachers
+
+  get 'needs_parent', :to => 'students#need_parent'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
