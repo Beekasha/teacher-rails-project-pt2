6272c55992ab5abb589fe80b02f289fd7bdf8d96
Beekasha <brian.j.kleiner@gmail.com>
2020-05-02 15:19:18 -0400

    getting the create parent to work

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -2,11 +2,11 @@ class ParentsController < ApplicationController
 
     def index
         if params[:student_id]
-            student = Student.find_by(id: params['student_id'] )
-            if student.parent.nil?
+            @student = Student.find_by(id: params['student_id'] )
+            if @student.parent.nil?
                 redirect_to students_path
             else
-                @parent = student.parent
+                @parent = @student.parent
             end
         else
             redirect_to students_path
@@ -15,7 +15,15 @@ class ParentsController < ApplicationController
 
     def new
         @parent = Parent.new
-        @parent.student_id = params['student_id']
+        @student = Student.find_by(id: params[:student_id])
+    end
+
+    def create
+        @student = Student.find_by(id: params[:student_id])
+
+        @parent = Parent.new
+        @parent.student_id = @student.id
+        redirect_to student_path(@student)
 
     end
 end
