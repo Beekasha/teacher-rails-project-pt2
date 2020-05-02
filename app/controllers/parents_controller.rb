87deac412a1f0567cd3235560a89943ad2987691
Beekasha <brian.j.kleiner@gmail.com>
2020-05-02 13:50:52 -0400

    parents set up, except creating new parents

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,9 +1,21 @@
 class ParentsController < ApplicationController
 
     def index
-  
-        student = Student.find_by(id: params["student_id"] )
-        @parent = student.parent
-        
+        if params[:student_id]
+            student = Student.find_by(id: params['student_id'] )
+            if student.parent.nil?
+                redirect_to students_path
+            else
+                @parent = student.parent
+            end
+        else
+            redirect_to students_path
+        end
+    end
+
+    def new
+        @parent = Parent.new
+        @parent.student_id = params['student_id']
+
     end
 end
