d7377af1d060261b46278b4841684215ac1b3cb5
Beekasha <brian.j.kleiner@gmail.com>
2020-05-08 11:36:21 -0400

    adding validators

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,9 @@
 class Parent < ApplicationRecord
     belongs_to :student
+    validates :phone_number, length: { is: 10 }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+
 
 
 end
